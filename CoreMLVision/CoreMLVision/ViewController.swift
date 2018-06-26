b711071d8a668e39953a5605eeb1c81d56c7f8d9
Jason Sanchez <sanchezjason@me.com>
2018-06-26 02:52:05 -0600

    Setting Up Vision Request

--- CoreMLVision/CoreMLVision/ViewController.swift
+++ CoreMLVision/CoreMLVision/ViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     private var imagePicker = UIImagePickerController()
+    
+    private var model = GoogLeNetPlaces()
     
     @IBOutlet weak var photoImageView: UIImageView!
     
@@ -22,6 +25,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
         dismiss(animated: true, completion: nil)
         guard let pickedImage = info[UIImagePickerControllerOriginalImage] as?
             UIImage else {
@@ -29,10 +33,32 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
         
         self.photoImageView.image = pickedImage
+        
+        processImage(image: pickedImage)
     }
     
+    private func processImage(image: UIImage) {
+        
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Unable to create the ciImage object")
+        }
+        guard let visionModel = try? VNCoreMLModel(for: self.model.model) else {
+            fatalError("Unable to create vision model")
+        }
+        
+        let visionRequest = VNCoreMLRequest(model: visionModel) {
+            request, error in
+        }
+        
+        let visionRequestHandler = VNImageRequestHandler(ciImage: ciImage, orientation: .up, options: [:])
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            try! visionRequestHandler.perform([visionRequest])
+        }
+}
+    
     @IBAction func openPhotoLibraryButtonPressed(_ sender: UIBarButtonItem) {
-        self.present(self.imagePicker, animated: true, completion: nil) 
+        self.present(self.imagePicker, animated: true, completion: nil)
     }
 }
 
